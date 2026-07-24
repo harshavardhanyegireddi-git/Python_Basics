5307c8e4ca9aaf9fec56e10383708049f4a44c80
Harsha Vardhan <harshavardhanyegireddi@gmail.com>
2026-07-24 14:31:18 +0530

    Modify Sets practice file.

--- Practice/16_Sets_Practice.py
+++ Practice/16_Sets_Practice.py
@@ -75,3 +75,79 @@ print("\n" + "=" * 50)
 
 # ---- 5.Solution ----
 
+numbers = {10, 20, 30, 40, 50}
+
+numbers.remove(40)
+
+print("After remove() :", numbers)
+
+print("\n" + "=" * 50)
+
+
+# ---- 6.Solution ----
+
+animals = {"Cat", "Dog", "Monkey"}
+
+animals.discard("Dog")
+
+print("After discard() :", animals)
+
+print("\n" + "=" * 50)
+
+
+# ---- 7.Solution ----
+
+numbers = {10, 20, 30, 40, 50}
+
+print("Lnegth of numbers :", len(numbers))
+
+print("\n" + "=" * 50)
+
+
+# ---- 8.Solution ----
+
+languages = {"Python", "Java", "C++"}
+
+print("Python in Set :", "Python" in languages)
+
+print("\n" + "=" * 50)
+
+
+# ---- 9.Solution ----
+
+fruits = {"Mango", "Orange", "Papaya", "Banana", "Apple"}
+
+for i in fruits:
+    print(i)
+
+print("\n" + "=" * 50)
+
+
+# ---- 10.Solution ----
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+print("Union :", set1.union(set2))
+
+print("\n" + "=" * 50)
+
+
+# ---- 11.Solution ----
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+print("Intersection :", set1.intersection(set2))
+
+print("\n" + "=" * 50)
+
+
+# ---- 12.Solution ----
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+print("Intersection :", set1.intersection(set2))
+
+print("\n" + "=" * 50)
